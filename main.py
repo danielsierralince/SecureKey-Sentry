62673f4719697e5e81e8a398ff4ea5bbd5845fb6
Daniel Sierra <danielsantiagosierralince@gmail.com>
2023-05-28 22:27:32 -0500

    DB Connection

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import hashlib
-import secrets
-import time
+import hashlib, secrets, time
 import tkinter as tk
+from pymongo import MongoClient
+
+#MongoDB Connection
+client = MongoClient("mongodb://localhost:27017/")
+db = client["proyectoEd"]
+collection = db["otp_collection"]
+
+#My DB
+my_host = "localhost"
+my_port = 27017
+my_uri = f"mongodb://{my_host}:{my_port}"
+
+#DB Access
+my_db = "SecureKey-Sentry"
+my_collect = "Users"
+
+client = MongoClient(my_uri)
+data_base = client[my_db]
+collection = data_base[my_collect]
 
 OTP = 0
 
@@ -63,7 +80,7 @@ def countdown(remaining_time):
         window.after(1000, countdown, remaining_time - 1)
 
 #Runs every minute to OTP change
-def update_password():
+def update_password(user):
     store_otp(user)
     password.set(str(otp_table[user][0]))
     countdown(60)
@@ -90,3 +107,48 @@ window.after(60000, update_password)
 
 #Starting the main loop
 window.mainloop()
+
+def md5_hash(pwd):
+    md5_hash = hashlib.md5(pwd.encode()).hexdigest()
+    return md5_hash
+
+class main_window():
+    def __init__(self):
+        #Login window settup
+        login_window = tk.Tk()
+        login_window.title("Log in")
+        login_window.geometry("200x150")
+
+        label_user = tk.Label(login_window, text="User:")
+        label_user.pack()
+
+        self.entry_user = tk.Entry(login_window)
+        self.entry_user.pack()
+
+        label_pwd = tk.Label(login_window, text="Password:")
+        label_pwd.pack()
+
+        self.entry_pwd = tk.Entry(login_window, show="*")
+        self.entry_pwd.pack()
+
+        self.message = tk.StringVar()
+        label_message = tk.Label(login_window, textvariable=self.message)
+        label_message.pack()
+
+        login_button = tk.Button(login_window, text="Log in", command=self.log_in)
+        login_button.pack()
+
+        #Loop init
+        login_window.mainloop()
+
+    def log_in(self):
+        #Get GUI data
+        usr = self.entry_user.get()
+        pwd = md5_hash(self.entry_pwd.get())
+
+        if collection.find_one({'user': usr, 'password hash': pwd}): #DB Search
+            self.message.set("Access granted!")
+        elif collection.find_one({'user': usr}): #Password validation
+            self.message.set("Incorrect password!")
+        elif not collection.find_one({'user': usr, 'password hash': pwd}): #User validation
+            self.message.set("User doesn't exist!")
